cdb91d8b18a0362f61964c2c7fc83a84e5afc5a0
Leebonggu <leebonggu12@hanmail.net>
2023-01-18 02:36:28 +0900

    Remove Security

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,3 +1,5 @@
+import { ACCESS_KEY } from './env.js';
+
 const BASE_URL = 'http://43.201.103.199/';
 
 const ROUTES = {
@@ -7,9 +9,6 @@ const ROUTES = {
 	EDIT: '/edit/:postId',
 };
 
-const ACCESS_KEY = 'cl6h-r3s_mgyE6pHW1aXKb5ievPDsCnm6_8x2KnqB_g';
-const SECRET_KEY = 'OwueTfuL2kKVQbz2ixIquwZGfK973m4a3FAmUTKMWNI';
-
 const IMAGE_URL = `https://api.unsplash.com/photos/random/?client_id=${ACCESS_KEY}`;
 
-export { BASE_URL, ROUTES, ACCESS_KEY, SECRET_KEY, IMAGE_URL };
+export { BASE_URL, ROUTES, IMAGE_URL };
